3eabe915bb0cad7d42de184a677c9a2724c7fb1f
SIUUU42 <trishaykaul@gmail.com>
2026-06-03 23:29:51 +0530

    feat: complete Day 3 card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,16 @@
 import './Card.css';
+export function Card({ children, ...props }) {
+  return <div className="card" {...props}>{children}</div>;
+}
 
-export const Card = () => {
-  return <div></div>;
+Card.Header = function CardHeader({ children }) {
+  return <div className="card-header">{children}</div>;
+};
+
+Card.Body = function CardBody({ children }) {
+  return <div className="card-body">{children}</div>;
+};
+
+Card.Footer = function CardFooter({ children }) {
+  return <div className="card-footer">{children}</div>;
 };
