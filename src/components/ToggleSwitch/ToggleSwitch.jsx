829757d157b548dda815cc97f20ad2227ac4800b
SIUUU42 <trishaykaul@gmail.com>
2026-06-08 23:33:04 +0530

    Created a toggle switch button which responds to user clicks

--- src/components/ToggleSwitch/ToggleSwitch.jsx
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -1,5 +1,18 @@
 import './ToggleSwitch.css';
 
-export const ToggleSwitch = () => {
-  return <div></div>;
-};
+export function ToggleSwitch({ id, label, defaultChecked, checked, onChange, disabled, ...props }) {
+  return (
+    <div className="toggle-switch" {...props}>
+      <input
+        type="checkbox"
+        id={id}
+        className="sr-only"
+        defaultChecked={defaultChecked}
+        checked={checked}
+        onChange={onChange}
+        disabled={disabled}
+      />
+      <label htmlFor={id}>{label}</label>
+    </div>
+  );
+}
